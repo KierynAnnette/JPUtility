549adab5aebf75ae3baefd7ef2d4a9b8cded5fb9
Kieryn <kieryn.annette@jerseypost.com>
2023-01-23 12:39:20 +0000

    welcome change

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -10,7 +10,7 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Jersey Post Utility" />
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -43,13 +43,16 @@ defineProps({
                         <div class="flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg>
                             <div class="ml-4 text-lg leading-7 font-semibold">
-                                <a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a>
+                                <a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">IPS Resend Data</a>
                             </div>
                         </div>
 
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                Sometimes there is a need to resend IPS data due to a failure on the first attempt or systems were down which caused to EDI to fail. Here you can regenerate EDI using 3 simple methods:
+                                <ul class="list-disc pt-5 ml-5">
+                                    <li>Via a CSV upload. You can download a csv template and populate it with the required data. There is also a query extract for Atlas.</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -58,7 +61,7 @@ defineProps({
                         <div class="flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-500"><path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" /></svg>
                             <div class="ml-4 text-lg leading-7 font-semibold">
-                                <a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Laracasts</a>
+                                <a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">CDS REsend</a>
                             </div>
                         </div>
 
